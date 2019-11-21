6873aa6682dec212d7c64190089c591791d9b619
qq348993363 <57035654+qq348993363@users.noreply.github.com>
2019-11-21 19:37:45 +0800

    2019.11.21

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+define('APIKEY', 'AIzaSyC5HdF-xARZgmFJTG3Jb5uLJ9V73iojv3c');
+define('GJ_CODE', 'HK');
+define('SITE_NAME', 'you2b1eclub');
+define('TITLENAME', 'you2beclub');
+define('EN2DEKEY', '32123wwerwertgfdgds');
+define('EMAIL', '1334347212@qq.com');
+define('NAME', 'liya');
+define('PASSWORD', '123');
+define('LOGINHTML', '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="Login.css"/>
+</head>
+<body>
+<center>
+    <p id="login">
+    <h1>µÇÂ½</h1>
+    <form method="post" style="margin:0px auto;">
+        <input type="text" required="required" placeholder="ÓÃ»§Ãû" name="name"></input>
+        <br>
+        <input type="password" required="required" placeholder="ÃÜÂë" name="password"></input>
+        <button class="but" type="submit">µÇÂ¼</button>
+    </form>
+    </p>
+</center>
+</body>
+</html>');
+?>
